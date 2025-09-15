4b5c659c1d6b7903f19bd5b7df819b2406f348f2
Mak-Work-Biz <makdabizuneh2005@gmail.com>
2025-09-15 17:54:45 +0300

    Create List comprehension.py

---
+++ List comprehension.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    number = []
+    for i in range(x + 1):
+        for j in range(y + 1):
+            for k in range(z + 1):
+                if i + j + k != n:        
+                    num = [i, j, k]        
+                    number.append(num)    
+    print(number)
